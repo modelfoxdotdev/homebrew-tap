01e560327de125ab7df68310dbd37212496ae0ce
David Yamnitsky <davidy@me.com>
2019-11-08 10:09:43 -0500

    alpha 3

--- tangram.rb
+++ tangram.rb
@@ -1,10 +1,9 @@
 class Tangram < Formula
-  desc "A command line tool to train and deploy machine learning models directly on your own computer."
+  desc "Add machine learning to your application in minutes."
   homepage "http://www.tangramhq.com"
-  url "/releases/latest"
-  sha256 "5690273415ece11d1a9e35629025701e2a6787dd2701e8821caf3920bd7d87ae"
-  version "0.1.0"
-
+  url "https://github.com/tangram-hq/tangram-dist/releases/download/v0.1.0-alpha.3/tangram-0.1.0-alpha.3-macos-x86_64.tar.gz"
+  sha256 "26dffd95d9e0b18054ac3d6c66020cb5849d2e716b955cef228b55e5aecbcacf"
+  version "0.1.0-alpha.3"
   def install
     bin.install "tangram"
   end
