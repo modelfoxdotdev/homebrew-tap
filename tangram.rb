355a86133398fbf0b66621d843914b6f24397759
David Yamnitsky <davidy@me.com>
2019-11-08 10:27:17 -0500

    fix sha

--- tangram.rb
+++ tangram.rb
@@ -2,7 +2,7 @@ class Tangram < Formula
   desc "Add machine learning to your application in minutes."
   homepage "http://www.tangramhq.com"
   url "https://github.com/tangram-hq/tangram-dist/releases/download/v0.1.0-alpha.3/tangram-0.1.0-alpha.3-macos-x86_64.tar.gz"
-  sha256 "26dffd95d9e0b18054ac3d6c66020cb5849d2e716b955cef228b55e5aecbcacf"
+  sha256 "5a1101dd36981b48d7d357843cf8fb04d86b8def6ad3cda50310e971968cfab6"
   version "0.1.0-alpha.3"
   def install
     bin.install "tangram"
