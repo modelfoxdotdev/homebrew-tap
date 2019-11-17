583230dae1f96e52454b8f7a422f961a061f0cca
David Yamnitsky <davidy@me.com>
2019-11-17 11:01:31 -0500

    alpha 6

---
+++ cli.rb
@@ -0,0 +1,10 @@
+class TangramCLI < Formula
+  desc "Machine learning for developers"
+  homepage "http://www.tangramhq.com"
+  url "https://github.com/tangram-hq/tangram-cli/releases/download/v0.1.0-alpha.6/tangram-cli-0.1.0-alpha.6-macos-x86_64.tar.gz"
+  sha256 "e873ffca71fb4c36eee2a6c8a4dba5d7249540edc47d7c00a234eecddfd35045"
+  version "0.1.0-alpha.6"
+  def install
+    bin.install "tangram"
+  end
+end
