45b5017456ab132a60fbaa930ae41152a669de4a
David Yamnitsky <david@yamnitsky.com>
2021-06-25 17:51:22 -0400

    v0.4.0

--- tangram.rb
+++ tangram.rb
@@ -1,16 +1,19 @@
 class Tangram < Formula
   desc "Tangram is an automated machine learning framework designed for programmers."
   homepage "https://www.tangram.xyz"
-  version "0.3.0"
+  version "0.4.0"
   if OS.mac? and Hardware::CPU.arm?
-    url "https://github.com/tangramxyz/tangram/releases/download/v0.3.0/tangram_cli_0.3.0_aarch64-apple-darwin.tar.gz"
-    sha256 "9c8bf56c9dcb898682c1a65c8c94e89c7d965aea040c1823f245bdd489dc3db0"
+    url "https://github.com/tangramxyz/tangram/releases/download/v0.4.0/tangram_cli_0.4.0_aarch64-apple-darwin.tar.gz"
+    sha256 "5e9bcc7249c63c94c8ccb73866847db1ace6690a0b51738d7075e503f14f8dc6"
   elsif OS.mac?
-    url "https://github.com/tangramxyz/tangram/releases/download/v0.3.0/tangram_cli_0.3.0_x86_64-apple-darwin.tar.gz"
-    sha256 "03211f9b2e2c9cf7892fbfdcf59f30c26104f4b4b3ab0138e49d8acff7f4b247"
+    url "https://github.com/tangramxyz/tangram/releases/download/v0.4.0/tangram_cli_0.4.0_x86_64-apple-darwin.tar.gz"
+    sha256 "0c124b9550205b88069e5be36344babc572acfd835e35fba61c44a075b757732"
+  elsif OS.linux? and Hard::CPU.arm?
+    url "https://github.com/tangramxyz/tangram/releases/download/v0.4.0/tangram_cli_0.4.0_aarch64-unknown-linux-gnu.tar.gz"
+    sha256 "1f03b735dd93d337c709ae7aad94626aa7077414c70ebb559d1784afebeae492"
   elsif OS.linux?
-    url "https://github.com/tangramxyz/tangram/releases/download/v0.3.0/tangram_cli_0.3.0_x86_64-unknown-linux-musl.tar.gz"
-    sha256 "64a528e98524dcd0266181d2874d104d30a1892300aca0b50f7171a4547e667d"
+    url "https://github.com/tangramxyz/tangram/releases/download/v0.4.0/tangram_cli_0.4.0_x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "bc225b86b1dfd905beee6cb97b6bf6a5a6b4d87dc97b64308ac081c49eddf176"
   end
   def install
     bin.install "tangram"
