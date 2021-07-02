1136229aa310de8bea152606c55230f6e395b649
David Yamnitsky <david@yamnitsky.com>
2021-07-02 13:50:44 -0400

    v0.5.0

--- tangram.rb
+++ tangram.rb
@@ -1,19 +1,19 @@
 class Tangram < Formula
   desc "Tangram is an automated machine learning framework designed for programmers."
   homepage "https://www.tangram.xyz"
-  version "0.4.0"
+  version "0.5.0"
   if OS.mac? and Hardware::CPU.arm?
-    url "https://github.com/tangramxyz/tangram/releases/download/v0.4.0/tangram_cli_0.4.0_aarch64-apple-darwin.tar.gz"
-    sha256 "5e9bcc7249c63c94c8ccb73866847db1ace6690a0b51738d7075e503f14f8dc6"
+    url "https://github.com/tangramxyz/tangram/releases/download/v0.5.0/tangram_cli_0.5.0_aarch64-apple-darwin.tar.gz"
+    sha256 "a09268c0301bce6701510e93cfac28ab2965de3e320fc2a1be7c9fe91459200b"
   elsif OS.mac?
-    url "https://github.com/tangramxyz/tangram/releases/download/v0.4.0/tangram_cli_0.4.0_x86_64-apple-darwin.tar.gz"
-    sha256 "0c124b9550205b88069e5be36344babc572acfd835e35fba61c44a075b757732"
+    url "https://github.com/tangramxyz/tangram/releases/download/v0.5.0/tangram_cli_0.5.0_x86_64-apple-darwin.tar.gz"
+    sha256 "7d0f03e6791e98595adb8c87e3d94d9aa64ace47ae23429057f3dce253def4bd"
   elsif OS.linux? and Hard::CPU.arm?
-    url "https://github.com/tangramxyz/tangram/releases/download/v0.4.0/tangram_cli_0.4.0_aarch64-unknown-linux-gnu.tar.gz"
-    sha256 "1f03b735dd93d337c709ae7aad94626aa7077414c70ebb559d1784afebeae492"
+    url "https://github.com/tangramxyz/tangram/releases/download/v0.5.0/tangram_cli_0.5.0_aarch64-unknown-linux-gnu.tar.gz"
+    sha256 "ffe6703fce9c374984769517e6ebcda9bb9bea87e7381c6d5103f643bcd4f987"
   elsif OS.linux?
-    url "https://github.com/tangramxyz/tangram/releases/download/v0.4.0/tangram_cli_0.4.0_x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "bc225b86b1dfd905beee6cb97b6bf6a5a6b4d87dc97b64308ac081c49eddf176"
+    url "https://github.com/tangramxyz/tangram/releases/download/v0.5.0/tangram_cli_0.5.0_x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "c31ccfd497ee3c97c82267afe67aeaa18d22aea97c2348a23834d04c80a7ab6a"
   end
   def install
     bin.install "tangram"
